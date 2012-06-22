6b98f193c40ca77d43712e6bb49499545461609a
eudaimos <jeff@hwgray.com>
2012-06-21 23:49:35 -0700

    added test cases no bodies

---
+++ base_tests.js
@@ -0,0 +1,19 @@
+function create_list() {
+}
+
+function create_item() {
+}
+
+function add_item_to_list() {
+}
+
+function add_item_to_other_list() {
+}
+
+function add_attr_to_list() {
+}
+
+function assign_attr_to_item() {
+}
+
+function 
